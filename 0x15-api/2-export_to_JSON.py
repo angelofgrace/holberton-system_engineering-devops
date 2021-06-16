18bd84fa37b92321942370d40aefd7d2052ae182
Grace Fallon <graceleefallon@gmail.com>
2021-06-16 20:27:16 +0000

    JSON export method

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Convert API requested info JSON export """
+
+import csv
+import requests
+import sys
+
+
+def save_tasks_to_json(employeeId):
+    """ By eId, export completed value of each task """
+
+    name = ''
+    userDict = {}
+
+    userRes = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employeeId))
+    todosRes = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.
+        format(employeeId))
+
+    name = userRes.json().get('username')
+    todosJson = todosRes.json()
+
+    for task in todosJson:
+        taskDict = {}
+        taskDict['task'] = task.get('title')
+        taskDict['username'] = name
+        taskDict['completed'] = task.get('completed')
+        
+        userDict[employeeId].append(taskDict)
+
+    with open("{}.csv".format(employeeId), 'w') as jsonFile:
+        json.dump(userDict, jsonFile)
+
+    return 0
+
+if __name__ == "__main__":
+    save_tasks_to_json(sys.argv[1])
